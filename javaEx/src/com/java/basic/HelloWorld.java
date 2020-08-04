b3a63300daa595682b9cfee20f1537b72b982cb1
Geonjoo Han <iwmism1938@naver.com>
2020-08-04 11:04:01 +0900

    첫 번째 commit

---
+++ javaEx/src/com/java/basic/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.java.basic;
+/*
+ * 화면에 Hello, World라 출력
+ */
+public class HelloWorld {
+	//직접 실행을 위해서 main method가 필요하다
+	public static void main(String[] args) {
+		// 출력코드
+		System.out.println("Hello, World");
+	}
+}
