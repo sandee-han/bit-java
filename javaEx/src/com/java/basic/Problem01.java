1ae69fd4a8d7de33aa2492255faa45b22192da3b
Geonjoo Han <iwmism1938@naver.com>
2020-08-11 12:49:18 +0900

    열거형, 배열 연습

--- javaEx/src/com/java/basic/Problem01.java
+++ javaEx/src/com/java/basic/Problem01.java
@@ -11,6 +11,9 @@ public class Problem01 {
 //		System.out.println("------------------------");
 //		quiz3();
 //		System.out.println("------------------------");
+//		quiz4();
+//		System.out.println("------------------------");
+
 	}
 
 	public static void quiz1() {
@@ -45,9 +48,19 @@ public class Problem01 {
 	}
 	
 	public static void quiz4() {
+		int num;
 		for(int i = 1; i<=10; i++) {
-		//	for(int j = 1; j<=)
+			num = i;
+			for(int j = 1; j<=10; j++) {
+				System.out.print(num + "\t");
+				num++;
+			}
+			System.out.println();
 		}
+	}
+	
+	public static void quiz5() {
+		
 	}
 	
 	
---
+++ javaEx/src/com/java/basic/reftypes/WeekDay.java
@@ -0,0 +1,20 @@
+package com.java.basic.reftypes;
+
+
+// 열거형
+// 값의 목록이 특정 값으로 제한되어 있을 때 활용
+// Java의 경우 상수, enum 많이 활용
+/* enum 활용
+		1. 코드 가독성 높아진다
+		2. 오류 가능성을 낮출 수 있다
+*/
+public enum WeekDay {
+	// 열거 상수: 모두 대문자로
+	SUNDAY,
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY
+}
