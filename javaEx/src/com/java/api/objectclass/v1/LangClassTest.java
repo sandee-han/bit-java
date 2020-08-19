0d762e8ad8e5c9cf31620953d4123c06447b96fa
Geonjoo Han <iwmism1938@naver.com>
2020-08-19 12:42:34 +0900

    Object 클래스

---
+++ javaEx/src/com/java/api/objectclass/v1/LangClassTest.java
@@ -0,0 +1,23 @@
+package com.java.api.objectclass.v1;
+
+public class LangClassTest {
+
+	public static void main(String[] args) {
+		Point p = new Point(10, 20);
+		
+		//	Java의 최상위 클래스는 Object
+		//	모든 클래스는 Object 클래스의 모든 기능을 물려받는다
+		//	Object로부터 물려받은 내용들
+		System.out.println(p.getClass().getSimpleName());
+		System.out.println(p.hashCode());	//	객체 식별코드(주소)
+		System.out.println(p.toString());
+		System.out.println(p);	//	toString() 코드와 동일
+		
+		//	부모 정보의 확인
+		System.out.println(p.getClass().getSuperclass().getName());
+		
+	}
+
+	
+
+}
