f642d856df20ca5f66d44d1ecad89d09804ccc3c
Geonjoo Han <iwmism1938@naver.com>
2020-08-19 11:37:13 +0900

    사용자 정의 예외 처리

--- javaEx/src/com/java/exception/ExceptionEx.java
+++ javaEx/src/com/java/exception/ExceptionEx.java
@@ -1,5 +1,6 @@
 package com.java.exception;
 
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -8,8 +9,34 @@ public class ExceptionEx {
 	public static void main(String[] args) {
 //		arithmeticExceptionEx();
 //		indexOutofBoundEx();
-		nullPointerExceptionEx();
+//		nullPointerExceptionEx();
+		customExceptionEx();
 	}
+	
+	private static void customExceptionEx() {
+		//	사용자 정의 예외의 활용
+		ThrowExcept ex = new ThrowExcept();
+		
+		try {
+//		ex.executeCheckedException();	//	CheckedException 발생
+//		ex.executeUncheckedException();	//	UncheckedExcepton 발생
+		System.out.println("10/0 = " + ex.divide(10, 0));
+//		} catch(IOException e) {
+//			System.err.println("예외 메세지: " + e.getMessage());
+		} catch(CustomArithmeticException e) {
+			//	본 예외는 RuntimeException의 자손이므로
+			//	RuntimeException보다 위에서 catch 해줘야 함
+			System.err.println("사용자 정의 예외 메세지: " + e.getMessage());
+			System.err.printf("예외 상황 발생시 num1 = %d, num2 = %d%n", e.getNum1(), e.getNum2());
+		} catch(RuntimeException e) {
+			System.err.println("런타임 예외 메세지: " + e.getMessage());
+		} 
+		
+		
+		
+	}
+	
+	
 	
 	private static void indexOutofBoundEx() {
 		int[] intArray = new int[] {3, 6, 9};
